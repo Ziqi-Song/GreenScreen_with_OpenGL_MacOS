62cfe3906881cdc749cc9544731762c823009361
Ziqi Song <songziqi1987@126.com>
2025-09-05 18:50:32 +0800

    Initial Commit

---
+++ GreenScreen_with_OpenGL_MacOS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GreenScreen_with_OpenGL_MacOS
+//
+//  Created by 宋子奇 on 2025/9/5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
